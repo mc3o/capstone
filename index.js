ab669d335d8c562ff5a4a0828faaeaf07f5a315b
Samuel Mbugua <mbugua.samuel@turnkeyafrica.com>
2020-03-06 17:07:36 +0300

    Ability to edit a teachers details

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {Client} = require('pg');
+const { Client } = require('pg');
 const path = require('path');
 const bodyParser = require('body-parser')
 var app = express();
 
 const connectionString = 'postgres://postgres:sec123@127.0.0.1:5432/school';
+// const connectionString = 'postgres://postgres:root@127.0.0.1:5435/school';
 const client = new Client({
     connectionString: connectionString
 });
@@ -13,47 +14,45 @@ client.connect();
 
 const port = process.env.PORT || 3000
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 // using callback
 
-app.get('/', async (req, res, ) =>{
+app.get('/', async(req, res, ) => {
     const rows = await getAllTeachers()
-    // res.setHeader("content-type", "application/json")
+        // res.setHeader("content-type", "application/json")
     res.render('home', {
         title: 'SM',
         teachers: rows
     })
-    
+
 });
 async function getAllTeachers() {
     const query = {
-        // give the query a unique name
-        name: 'fetch-user',
-        text: 'SELECT * FROM teachers'
-      }// to avoid sql injection
-   try {
-       const results =  await client.query(query)        
+            // give the query a unique name
+            name: 'fetch-user',
+            text: 'SELECT * FROM teachers'
+        } // to avoid sql injection
+    try {
+        const results = await client.query(query)
 
         return results.rows; // to return all records result.rows
-   }
-   catch (e) {
-       return []
-   }
-   
+    } catch (e) {
+        return []
+    }
+
 }
 
-app.get('/', async (req, res) => {
+app.get('/', async(req, res) => {
     let result = {}
     try {
-        const {id} = req.body
+        const { id } = req.body
         await getUserById(id)
         result.success = true
-    }
-    catch(e){
+    } catch (e) {
         result.success = false
     }
 })
@@ -69,29 +68,28 @@ async function insertTeacher(id, name, age) {
     const query1 = {
         text: 'INSERT INTO teachers(id, name, age) VALUES($1, $2, $3)',
         values: [id, name, age]
-      }
-       await client.query(query1)        
-       return true    
-}
-app.post('/add', async (req, res) => {
-    let result = {}
-   
-    try {
-        const {id, name, age} = req.body
-        await insertTeacher(id, name, age)
-        
-        res.redirect('/')
-           
     }
-    catch(e) {
+    await client.query(query1)
+    return true
+}
+app.post('/add', async(req, res) => {
+    let result = {}
+
+    try {
+        const { id, name, age } = req.body
+        await insertTeacher(id, name, age)
+
+        res.redirect('/')
+
+    } catch (e) {
         res.send(e)
         result.success = false
     }
-   
+
 
 })
 
-async function deleteTeacher(id){
+async function deleteTeacher(id) {
     const query = {
         text: "DELETE FROM teachers WHERE id = $1",
         values: [id]
@@ -99,14 +97,13 @@ async function deleteTeacher(id){
     await client.query(query)
     return true
 }
-app.delete('/delete/:id', async (req, res) => {
+app.delete('/delete/:id', async(req, res) => {
     let result = {}
     try {
-        const {id} = req.params
+        const { id } = req.params
         await deleteTeacher(id)
         res.redirect('/')
-    }
-    catch(e) {
+    } catch (e) {
         res.send(e)
         result.success = false
     }
@@ -114,24 +111,23 @@ app.delete('/delete/:id', async (req, res) => {
 
 async function updateTeacher(id, name, age) {
     const query = {
-        // give the query a unique name
-        name: 'fetch-user',
-        text: 'UPDATE teachers SET id =$1, name =$2, age=$3 WHERE id =$1',
-        values: [id, name, age]
-      }// to avoid sql injection
-      await client.query(query)
-      return true       
+            // give the query a unique name
+            name: 'fetch-user',
+            text: 'UPDATE teachers SET id =$1, name =$2, age=$3 WHERE id =$1',
+            values: [id, name, age]
+        } // to avoid sql injection
+    await client.query(query)
+    return true
 }
-app.put('/edit', async (req, res) => {
-    try{
-        let {id, name, age} = req.body
+app.put('/edit', async(req, res) => {
+    try {
+        let { id, name, age } = req.body
         await updateTeacher(id, name, age)
         res.redirect('/')
-    }
-    catch(e){
+    } catch (e) {
         res.send(e)
-    } 
-    
+    }
+
 })
 
 
@@ -140,5 +136,3 @@ app.put('/edit', async (req, res) => {
 app.listen(port, (req, res) => {
     console.log(`listening on port ${port}`)
 })
-
-
--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,36 @@
 var delButton = document.querySelector('#delete-button');
-if(delButton) {
+if (delButton) {
     delButton.addEventListener('click', (e) => {
         let id = delButton.dataset.id;
         let url = `/delete/${id}`
         if (confirm('Delete Recipe')) {
             $.ajax({
-                url:url,
-                type: 'DELETE',
-                success: function(result){
-                    console.log('deleted ')
-                    window.location.href = '/'
-                },
-                error: function(err){
-                    console.log(err)
-                }
-            })
-        //     let xhr = new XMLHttpRequest()
-        //     xhr.open('post', url)
-        //     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        //     xhr.send()
-        //     xhr.onload = function() {
-        //         if (xhr.status != 200) { // analyze HTTP status of the response
-        //             console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-        //           } else { // show the result
-        //             console.log(`Done, got ${xhr.response.length} bytes`); // responseText is the server
-        //           }
-        //     }
-         }
+                    url: url,
+                    type: 'DELETE',
+                    success: function(result) {
+                        console.log('deleted ')
+                        window.location.href = '/'
+                    },
+                    error: function(err) {
+                        console.log(err)
+                    }
+                })
+                //     let xhr = new XMLHttpRequest()
+                //     xhr.open('post', url)
+                //     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+                //     xhr.send()
+                //     xhr.onload = function() {
+                //         if (xhr.status != 200) { // analyze HTTP status of the response
+                //             console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+                //           } else { // show the result
+                //             console.log(`Done, got ${xhr.response.length} bytes`); // responseText is the server
+                //           }
+                //     }
+        }
         // console.log(url)
 
-      });
-}else {
+    });
+} else {
     console.log('none')
 }
 let editButton = document.querySelector('#edit-button');
@@ -38,14 +38,27 @@ editButton.addEventListener('click', function(e) {
     let id = e.target.dataset.id;
     // alert(e.target.getAttribute("data-name"));
 
-    let name = e.target.dataset.name;
-    let age = e.target.dataset.age;
-    console.log(name)
-    document.getElementById('teachId').value = id;
-    document.getElementById('teachName').value = name;
-    document.getElementById('teachAge').value = age
+    // let name = e.target.dataset.name;
+    // let age = e.target.dataset.age;
+    // console.log(name)
+    // document.getElementById('teachId').value = id;
+    // document.getElementById('teachName').value = name;
+    // document.getElementById('teachAge').value = age
 
 })
+
+/**
+ * The data should be passed as a string and parsed back to json for processing
+ * Check for nulls else JSON.parse throws a null pointer
+ * @param {*} data 
+ */
+function editTeacher(data) {
+    var teacher = JSON.parse(data)
+    document.getElementById('teachId').value = teacher.id;
+    document.getElementById('teachName').value = teacher.name;
+    document.getElementById('teachAge').value = teacher.age
+
+}
 
 // addButton.addEventListener('click', () => {
 //     alert('add')
